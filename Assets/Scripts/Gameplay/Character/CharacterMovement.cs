e3f20025ad48e01b94ecaa174208b406923c43a9
Andrey Boronnikov <boronnikov_a_i@mail.ru>
2025-09-26 10:55:28 +0300

    Перемещение персонажа кликом по NavMesh в тупую

---
+++ Assets/Scripts/Gameplay/Character/CharacterMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Gameplay.Character
+{
+	[RequireComponent(typeof(NavMeshAgent))]
+	public class CharacterMovement : MonoBehaviour
+	{
+		[SerializeField] private Camera _mainCamera;
+
+		private NavMeshAgent _agent;
+
+		private void Update()
+		{
+			if (!Input.GetMouseButtonDown(0))
+				return;
+
+			var ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast(ray, out var hit))
+			{
+				_agent.SetDestination(hit.point);
+			}
+		}
+
+#if UNITY_EDITOR
+		private void OnValidate()
+		{
+			if (_agent == null)
+				_agent = GetComponent<NavMeshAgent>();
+		}
+#endif
+	}
+}
